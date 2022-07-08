533db25f558de0e02f5f7584ea90e8aa919e213c
chiragautomotive <95574219+chiragautomotive@users.noreply.github.com>
2022-07-08 16:44:43 +0530

    initial update
    v 1.0

---
+++ index.js
@@ -0,0 +1,7 @@
+function getPhoneNumber(){
+            // Selecting the input element and get its value 
+            var inputVal = 91+document.getElementById("phoneNumber").value;
+        var walink ="https://wa.me/"+inputVal
+ 
+  window.open("https://wa.me/"+inputVal+"?text=Greetings%20Dear%20Member%2C%0AOur%20team%20Chirag%20Automotive%20Congratulates%20you%20on%20your%20vehicle%20ownership.%0AIts%20our%20immense%20pleasure%20for%20you%20to%20be%20part%20of%20our%20EV%20Family.%0AIf%20you%20ever%20need%20any%20assistance%20with%20your%20vehicle%20maintenance%20or%20any%20queries%2C%20please%20let%20us%20know.%0A%0AAt%20Your%20Service%2C%20%0AAlways%2C%0ATeam%20CHIRAG%20AUTOMOTIVE")
+}
